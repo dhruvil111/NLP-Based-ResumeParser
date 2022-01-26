bd1973005de3d8a76ff86dff81acd1ff25bbc0f7
dhruvil111 <46847345+dhruvil111@users.noreply.github.com>
2022-01-27 00:15:18 +0530

    Parsing Functionality added

--- RPA/parser_app/models.py
+++ RPA/parser_app/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
 class myuploadfiles(models.Model):
-    myfiles = models.FileField(upload_to="media")
+    myfiles = models.FileField('Resume', upload_to="media")
+    name          = models.CharField('Name', max_length=255, null=True, blank=True)
+    email         = models.CharField('Email', max_length=255, null=True, blank=True)
+    mobile_number = models.CharField('Mobile Number',  max_length=255, null=True, blank=True)
+    education     = models.CharField('Education', max_length=255, null=True, blank=True)
+    skills        = models.CharField('Skills', max_length=1000, null=True, blank=True)
+    company_name  = models.CharField('Company Name', max_length=1000, null=True, blank=True)
+    college_name  = models.CharField('College Name', max_length=1000, null=True, blank=True)
+    designation   = models.CharField('Designation', max_length=1000, null=True, blank=True)
+    experience    = models.CharField('Experience', max_length=1000, null=True, blank=True)
+    #uploaded_on   = models.DateTimeField('Uploaded On', auto_now_add=True)
+    total_experience  = models.CharField('Total Experience (in Years)', max_length=1000, null=True, blank=True)
--- RPA/parser_app/views.py
+++ RPA/parser_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
-from . models import myuploadfiles
+from .models import myuploadfiles
+from pyresparser import ResumeParser
+from django.conf import settings
+import os
 
 def index(request):
     return render(request,"index.html")
@@ -9,6 +12,51 @@ def send_files(request):
         myfile = request.FILES.getlist("UploadFiles")
 
         for f in myfile:
-            myuploadfiles(myfiles = f).save()
+            #myuploadfiles(myfiles = f)
+            resume = myuploadfiles(myfiles = f)
+            resume.save()
+            parser = ResumeParser(os.path.join(settings.MEDIA_ROOT, resume.myfiles.name))
 
-        return HttpResponse("Files Uploaded Successfully")
+            data = parser.get_extracted_data()
+            resume.name               = data.get('name')
+            resume.email              = data.get('email')
+            resume.mobile_number      = data.get('mobile_number')
+            resume.company_name      = data.get('company_names')
+            resume.college_name       = data.get('college_name')
+            l = []
+            if data.get('designation'):
+                for i in data.get('designation'):
+                    if '@' in i:
+                        continue
+                    else:
+                        l.append(i)
+
+            resume.designation        = l
+            resume.total_experience   = data.get('total_experience')  
+
+            if data.get('skills') is not None:
+                resume.skills         = ', '.join(data.get('skills'))
+            else:
+                resume.skills         = None 
+            
+            if data.get('degree') is not None:
+                resume.education      = ', '.join(data.get('degree'))
+            else:
+                resume.education      = None
+            
+            if data.get('experience') is not None:
+                resume.experience     = ', '.join(data.get('experience'))
+            else:
+                resume.experience     = None
+            
+            resume.save()
+            resumes = myuploadfiles.objects.all()
+            context = {
+                'resumes': resumes,
+            }
+            
+
+           
+
+        return render(request,'UI.html',context)
+        #return HttpResponse("Files Uploaded Successfully")
